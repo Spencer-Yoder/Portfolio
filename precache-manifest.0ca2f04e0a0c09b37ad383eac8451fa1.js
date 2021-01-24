c4457d41a915f9047e17faa74f56d0a7b967eee6
Spencer Yoder <sryoder8@yahoo.com>
2021-01-24 16:55:07 -0600

    Updates

---
+++ precache-manifest.0ca2f04e0a0c09b37ad383eac8451fa1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70eab2849c9950e066cfd89820cf6cb4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0fa4b10b6ce48ae662e9",
+    "url": "/portfolio/static/css/main.b1a9fde2.chunk.css"
+  },
+  {
+    "revision": "4cd07b49f31a11f1702f",
+    "url": "/portfolio/static/js/2.b4587bc1.chunk.js"
+  },
+  {
+    "revision": "8656c31a83eb684317501013da8fff57",
+    "url": "/portfolio/static/js/2.b4587bc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa4b10b6ce48ae662e9",
+    "url": "/portfolio/static/js/main.2f2c0e99.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "45ffa6f3d2075806bb1da63ff1b1448f",
+    "url": "/portfolio/static/media/close.45ffa6f3.svg"
+  },
+  {
+    "revision": "2a7860b62b7f5e43cbf9a4a7d56ed91c",
+    "url": "/portfolio/static/media/download.2a7860b6.svg"
+  },
+  {
+    "revision": "0f7f243046311562d0c11e67cd8218f6",
+    "url": "/portfolio/static/media/linkedin.0f7f2430.svg"
+  },
+  {
+    "revision": "d779edcf14f4a5ea01a4b3fff5553d68",
+    "url": "/portfolio/static/media/masplay.d779edcf.png"
+  },
+  {
+    "revision": "8c18a2d728017eea8de0f11776ebb4d7",
+    "url": "/portfolio/static/media/menu.8c18a2d7.svg"
+  },
+  {
+    "revision": "dc8e0e513d3b5834261bfc67f3ad5f5b",
+    "url": "/portfolio/static/media/pihole.dc8e0e51.png"
+  }
+]);
