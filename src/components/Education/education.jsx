a25ab22d8ec466b0612bf45ab883f6b8520d9466
Spencer Yoder <sryoder8@yahoo.com>
2021-01-25 19:42:12 -0600

    Updated styles

--- src/components/Education/education.jsx
+++ src/components/Education/education.jsx
@@ -10,15 +10,16 @@ const Education = () => {
       <SectionTitle title={"Education"} />
       <div className="education-container">
         <div className="education-text">
-          <p className="edu-p p1">Northern Illinois University</p>
-          <p className="edu-p p2">
+          <p className="edu-p1">Northern Illinois University</p>
+          <p className="edu-p2">
             Bachelor of Science - Computer Science <span>•</span> December 2018
           </p>
-          <p3 className="edu-p p3">
+          <p className="edu-p3">
             Concentrated on the skills needed to be a software developer,
             including programming in several languages, database management, and
-            software engineering.
-          </p3>
+            software engineering. Created many different projects that contained
+            frontend and backend applications.
+          </p>
         </div>
       </div>
     </div>
